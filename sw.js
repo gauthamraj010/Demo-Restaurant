c0e3c0fbfff84be5d55c786bb2399d4bb8398518
gauthamraj010 <43185881+gauthamraj010@users.noreply.github.com>
2026-06-29 21:55:19 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-// Background pipeline enabling application install compliance
-self.addEventListener('fetch', function(event) {
-    // Left empty intentionally to pass browser install criteria
-});

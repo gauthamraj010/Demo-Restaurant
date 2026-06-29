27b513a614ec984f1176f57cf3df8e0d7de3702a
gauthamraj010 <43185881+gauthamraj010@users.noreply.github.com>
2026-06-29 09:37:24 +0530

    Create basic service worker for PWA
    Add a basic service worker for PWA capabilities.

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Basic service worker to enable PWA installation capabilities
+self.addEventListener('fetch', function(event) {
+    // You can leave this blank or use it to cache your app files later
+});
